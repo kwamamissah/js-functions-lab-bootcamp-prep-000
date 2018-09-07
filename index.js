044e34f074417b5b9e1a19db90c0a739b6b5cec4
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-07 18:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!';
+  return `Happy ${holiday}, ${name}!`; 
 }
 
 function holidayCountdown(holiday, days) {
-  return "It\'s ${days} days until ${holiday}!";
+  return `It\'s ${days} days until ${holiday}!`;
 }
 
-happyHolidaysTo("you");
-happyHolidayTo("Happy Independence Day", "you");
-holidayCountdown("Mother\'s Day", 20);
